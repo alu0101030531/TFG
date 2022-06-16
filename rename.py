88c600ba4e10f3272673005138497d0e2799f59b
Christian Pérez <alu0101030531@ull.edu.es>
2022-06-16 11:18:25 +0100

    removed files

--- rename.py
+++
@@ -1,9 +0,0 @@
-import os
-
-def rename(path):
-  name = 6006
-  for filename in os.listdir(path):
-    os.rename(path + "/" + filename, "test/" + str(name) + ".png")
-    name += 1
-
-rename("test2")
